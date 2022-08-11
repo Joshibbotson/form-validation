51a1b8b7d4f9c7707447471a7d51782388ae6b00
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-08-11 20:46:40 +0100

    new feat- added validation for email and country

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = {
+    email: document.getElementById("email"),
+    country: document.getElementById("country"),
+    zipcode: document.getElementById("zipCode"),
+    password: document.getElementById("password"),
+    passwordConfirmation: document.getElementById("passwordConfirmation"),
+}
+validateForm()
+function validateForm() {
+    form.email.addEventListener("blur", validateEmail)
+
+    function validateEmail() {
+        console.log("blurred")
+        if (form.email.validity.typeMismatch) {
+            form.email.setCustomValidity("Please enter a valid email address")
+            return form.email.reportValidity()
+        } else {
+            form.email.setCustomValidity("")
+            return form.email.reportValidity()
+        }
+    }
+
+    form.country.addEventListener("click", validateCountry)
+
+    function validateCountry() {
+        if (form.country.value === "") {
+            form.country.setCustomValidity("Please enter a country")
+            return form.country.reportValidity()
+        } else {
+            form.country.setCustomValidity("")
+            return form.country.reportValidity()
+        }
+    }
+}
