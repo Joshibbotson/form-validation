97dca0ab59414d25689c51b436afb9ea15c05bdd
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-08-13 17:53:11 +0100

    new feat- finished password validation

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ const form = {
     password: document.getElementById("password"),
     passwordConfirmation: document.getElementById("passwordConfirmation"),
 }
-
 validateForm()
 function validateForm() {
-    form.email.addEventListener("blur", validateEmail)
+    form.email.addEventListener("input", validateEmail)
+    form.country.addEventListener("click", validateCountry)
+    form.country.addEventListener("input", validateCountry)
+    form.zipcode.addEventListener("input", validateZipCode)
+    form.password.addEventListener("input", () => {
+        return validatePassword(form.password.value)
+    })
 
     function validateEmail() {
         console.log("blurred")
@@ -22,8 +27,6 @@ function validateForm() {
         }
     }
 
-    form.country.addEventListener("click", validateCountry)
-
     function validateCountry() {
         if (form.country.value === "") {
             form.country.setCustomValidity("Please enter a country")
@@ -33,10 +36,62 @@ function validateForm() {
             return form.country.reportValidity()
         }
     }
+
+    function validateZipCode() {
+        const isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/
+        if (isValidZip.test(form.zipcode.value) === false) {
+            form.zipcode.setCustomValidity("Please enter a valid zip code")
+            return form.zipcode.reportValidity()
+        } else {
+            form.zipcode.setCustomValidity("")
+            return form.zipcode.reportValidity()
+        }
+    }
+
+    function validatePassword(password) {
+        if (password.length < 8) {
+            form.password.setCustomValidity("Too short")
+            return form.password.reportValidity()
+        }
+        const hasUpperCase = /[A-Z]/.test(password)
+        const hasLowerCase = /[a-z]/.test(password)
+        const hasNumbers = /\d/.test(password)
+        const hasNonalphas = /\W/.test(password)
+        if (hasUpperCase + hasLowerCase + hasNumbers + hasNonalphas < 4) {
+            form.password.setCustomValidity(
+                "Make sure to have a capital, lower case, number and special character"
+            )
+            return form.password.reportValidity()
+        } else {
+            form.password.setCustomValidity("")
+            return form.password.reportValidity()
+        }
+    }
+
+    if (
+        validateEmail() === true &&
+        validateCountry() === true &&
+        validateZipCode() === true
+    ) {
+        return true
+    } else {
+        return false
+    }
 }
 
 form.form.addEventListener("submit", e => {
     const messages = []
     // put logic to push error messages into messages
-    e.preventDefault()
+
+    switch (validateForm()) {
+        case false:
+            console.log("hi")
+
+            e.preventDefault()
+            break
+        case true:
+            console.log("bye")
+
+            return
+    }
 })
